7f19da22943776b36b8f3d770ccd8e622fa73ec6
gabrieldomail <gabrieldomail@gmail.com>
2025-11-10 13:39:28 -0300

    Reparacion2

---
+++ move_files.bat
@@ -0,0 +1,28 @@
+move ARKANOID.jpg images\
+move arkanoid.webp images\
+move "flappy bird.jpg" images\
+move Image_game.jpg images\
+move pacman.jfif images\
+move PACMAN.jpg images\
+move PACMAN.png images\
+move PUBLISH_LOGOS_Rappia.png images\
+move rappia_logo_inicio_page.png images\
+move rappia_logo_tokenomics.PNG images\
+move RAPPIA-2.png images\
+move rappibellion_el_ultimo_tango_inicio_page.png images\
+move rappibellion_logo_movil.png images\
+move RB1.jpg images\
+move RB3.jpg images\
+move RB4.jpg images\
+move snake.PNG images\
+move "space invaders.jpg" images\
+move SPACE_INVADERSII.png images\
+move tetris.JPG images\
+move zorzal.PNG images\
+move por-una-cabeza.mp3 sounds\
+move "whitepaper_rappibellion.pdf" docs\
+move "WHITEPAPER$RPPI (Tokenomicon v1.0) .pdf" docs\
+move confetti.browser.min.js.descarga js\
+move firebase-app-compat.js.descarga js\
+move firebase-database-compat.js.descarga js\
+move "juegos beta\*" images\
