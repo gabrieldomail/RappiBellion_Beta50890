0dec8b084f38d0aefe944556df9dce1f292edec6
gabrieldomail <gabrieldomail@gmail.com>
2026-04-01 12:29:59 -0300

    Update USDT address in production config

--- web3-config.js
+++ web3-config.js
@@ -29,7 +29,7 @@ class Web3Config {
 
         // ── USDT en Optimism (modo producción) ──────────────────────────────
         this.USDT_CONFIG = {
-            address: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
+            address: '0x91a25d42AcA05858f692CaC03Cb20BFAC9315732',
             abi: [
                 "function balanceOf(address owner) view returns (uint256)",
                 "function transfer(address to, uint256 amount) returns (bool)",
